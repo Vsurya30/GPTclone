7782666af6205dbdcfd60bc7774a946c2122ff71
SURYA.V <91141273+Vsurya30@users.noreply.github.com>
2023-06-06 00:39:14 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        chatblack: {50 : '#343541'}
+  
+      }
+    },
+  },
+  plugins: [],
+}
+
